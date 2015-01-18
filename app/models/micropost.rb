821aca964f5ed212142bde3060fc98b8a55afeb9
Lorenzo Sanchez <lsanc104@fiu.edu>
2015-01-18 15:59:25 -0500

    updates

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -1,7 +1,6 @@
 class Micropost < ActiveRecord::Base
   belongs_to :user
-
+  default_scope -> { order(created_at: :desc) }
   validates :user_id, presence: true
-  validates :content, prensence: true, lenght { maximum: 140}
-  
+  validates :content, presence: true, length: { maximum: 140 }
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150118034538) do
+ActiveRecord::Schema.define(version: 20150118154037) do
 
   create_table "microposts", force: :cascade do |t|
     t.text     "content",    limit: 65535
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(version: 20150118034538) do
     t.datetime "updated_at",               null: false
   end
 
+  add_index "microposts", ["user_id", "created_at"], name: "index_microposts_on_user_id_and_created_at", using: :btree
   add_index "microposts", ["user_id"], name: "index_microposts_on_user_id", using: :btree
 
   create_table "users", force: :cascade do |t|
@@ -33,6 +34,8 @@ ActiveRecord::Schema.define(version: 20150118034538) do
     t.string   "activation_digest", limit: 255
     t.boolean  "activated",         limit: 1,   default: false
     t.datetime "activated_at"
+    t.string   "reset_digest",      limit: 255
+    t.datetime "reset_sent_at"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
