cf9dd5ccaf693cb50e28360a26dcbd8010e74a0e
Lorenzo Sanchez <lsanc104@fiu.edu>
2015-01-18 11:18:21 -0500

    updates

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150118050716) do
+ActiveRecord::Schema.define(version: 20150118034538) do
+
+  create_table "microposts", force: :cascade do |t|
+    t.text     "content",    limit: 65535
+    t.integer  "user_id",    limit: 4
+    t.datetime "created_at",               null: false
+    t.datetime "updated_at",               null: false
+  end
+
+  add_index "microposts", ["user_id"], name: "index_microposts_on_user_id", using: :btree
 
   create_table "users", force: :cascade do |t|
     t.string   "name",              limit: 255
@@ -24,8 +33,6 @@ ActiveRecord::Schema.define(version: 20150118050716) do
     t.string   "activation_digest", limit: 255
     t.boolean  "activated",         limit: 1,   default: false
     t.datetime "activated_at"
-    t.string   "reset_digest",      limit: 255
-    t.datetime "reset_sent_at"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
