435db9f38f00eac9a090a333bd9f21029a929ff0
Lorenzo Sanchez <lsanc104@fiu.edu>
2015-01-17 22:12:37 -0500

    updates

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -10,7 +10,6 @@ module SessionsHelper
     if(user_id = session[:user_id])
       @current_user ||= User.find_by(id: session[:user_id])
     elsif(user_id = cookies.signed[:user_id])
-      raise       # The tests still pass, so this branch is currently untested.
       user = User.find_by(id: user_id)
       if user && user.authenticate?(cookies[:remember_token])
         log_in user
@@ -36,9 +35,25 @@ module SessionsHelper
     cookies.permanent[:remember_token] = user.remember_token
   end
   
+  #Returns true if the given user is the current user.
+  def current_user?(user)
+    user == current_user
+  end
+
   def forget(user)
     user.forget
     cookies.delete(:user_id)
     cookies.delete(:remember_token)
+  end
+  
+    # Redirects to stored location (or to the default).
+  def redirect_back_or(default)
+    redirect_to(session[:forwarding_url] || default)
+    session.delete(:forwarding_url)
+  end
+
+  # Stores the URL trying to be accessed.
+  def store_location
+    session[:forwarding_url] = request.url if request.get?
   end
 end
